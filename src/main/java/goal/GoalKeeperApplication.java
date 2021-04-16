d73874e76be46989bd2b61b4b8f5b1b89dd61402
DS <DS@603-13>
2021-04-16 14:58:38 +0900

    1.0.0

---
+++ src/main/java/goal/GoalKeeperApplication.java
@@ -0,0 +1,13 @@
+package goal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalKeeperApplication.class, args);
+	}
+
+}
