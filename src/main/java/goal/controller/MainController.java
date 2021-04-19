ff8a83b28a7f6360b37e1458f7ae25729a77e3f7
jm6415 <81296315+jm6415@users.noreply.github.com>
2021-04-19 18:21:51 +0900

    controller 0.0.1

--- src/main/java/goal/controller/MainController.java
+++ src/main/java/goal/controller/MainController.java
@@ -1,15 +1,31 @@
 package goal.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import goal.vo.userVO;
+
 @RestController
 public class MainController {
 	
+//	@RequestMapping("/home")
+//	public ModelAndView openHome() {
+//		ModelAndView mv = new ModelAndView("/view/main_header");
+//		return mv;
+//	}
+
 	@RequestMapping("/home")
-	public ModelAndView openHome() {
-		ModelAndView mv = new ModelAndView("/view/main_header");
-		return mv;
+	public ModelAndView openHome(@ModelAttribute userVO vo,HttpSession session) {
+		boolean result = false;
+		if(result == false) {
+			
+		} else {
+			
+		}
+		return null;
 	}
 }
