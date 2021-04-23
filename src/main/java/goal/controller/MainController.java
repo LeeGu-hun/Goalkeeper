eba3bfca4649252fbf632612f9ba4aaed8aec672
jm6415 <81296315+jm6415@users.noreply.github.com>
2021-04-23 12:34:42 +0900

    controller 수정

--- src/main/java/goal/controller/MainController.java
+++ src/main/java/goal/controller/MainController.java
@@ -3,7 +3,9 @@ package goal.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,20 +17,27 @@ import goal.vo.UserVO;
 @RestController
 public class MainController {
 	
-	
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping("/home")
+	@GetMapping("/home")
 	public ModelAndView openHome(@ModelAttribute UserVO vo,HttpSession session) {
-
+		ModelAndView mv = new ModelAndView("/view/main_header_login");
+		return mv;
+	}
+	
+	@PostMapping("/home")
+	public ModelAndView loginCheck(@ModelAttribute UserVO vo,HttpSession session) {
 		boolean result = userService.checkLogin(vo);
 		ModelAndView mv = new ModelAndView("/view/main_header_login");
+		UserVO user = userService.loginUser(vo);
 		if(result == false) {
-			
+			mv.addObject("msg", "fail");
 		} else {
-			
+			session.setAttribute("user", vo);
+			mv.addObject("msg", "success");
 		}
 		return mv;
 	}
+	
 }
