45b29d9f5638ab1306236b39669afbf86acd342d
DS <DS@603-18>
2021-04-30 15:31:08 +0900

    myPage ....

--- src/main/java/goal/mapper/FriendMapper.java
+++ src/main/java/goal/mapper/FriendMapper.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import goal.vo.FriendVO;
+import goal.vo.UserVO;
 
 @Mapper
 public interface FriendMapper {
-	public List<FriendVO> listFriend(int uno);
+	public List<FriendVO> listFriend(UserVO vo);
 	
 	public int addFriend(FriendVO vo);
 	
-	public int deleteFriend(int fno);
+	public int deleteFriend(int uno);
 }
