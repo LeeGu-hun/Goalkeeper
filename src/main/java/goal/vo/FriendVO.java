bf9d95efb61015189c31b20b6505bb3cf6ff279c
DS <DS@603-18>
2021-04-30 16:11:53 +0900

    myPage..

--- src/main/java/goal/vo/FriendVO.java
+++ src/main/java/goal/vo/FriendVO.java
@@ -2,15 +2,17 @@ package goal.vo;
 
 import java.util.Date;
 
+import org.apache.ibatis.type.Alias;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
 @Data
+@Alias("friendVO")
 public class FriendVO {
 	private int uno, fno;
 	private String f_name;
-	private String f_numbrr;
+	private String f_number;
 	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date f_birthdate;
