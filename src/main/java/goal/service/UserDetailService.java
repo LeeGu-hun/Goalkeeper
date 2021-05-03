9836584790fe2e8bd9d9271a52b05ef92a06d9a9
ds606 <ds606@603-24>
2021-05-03 15:55:52 +0900

    수정

--- src/main/java/goal/service/UserDetailService.java
+++ src/main/java/goal/service/UserDetailService.java
@@ -14,26 +14,27 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import goal.repository.UserRepository;
+
 import goal.vo.UserVO;
 import lombok.AllArgsConstructor;
 
-@Service
-public class UserDetailService implements UserDetailsService{
-	/*
-	 * @Autowired private UserRepository user;
-	 * 
-	 * @Autowired private PasswordEncoder passwordEncoder;
-	 * 
-	 * @Override public UserDetails loadUserByUsername(String u_id) throws
-	 * UsernameNotFoundException { UserVO user = this.user.findById(u_id);
-	 * List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-	 * authorities.add(new SimpleGrantedAuthority("USER"));
-	 * 
-	 * return new User(user.getUserId(), user.getUserPw(), authorities); }
-	 * 
-	 * public UserVO save(UserVO vo) {
-	 * vo.setUserPw(passwordEncoder.encode(vo.getUserPw())); return user.save(vo); }
-	 */
-	
-}
+/*
+ * @Service public class UserDetailService implements UserDetailsService{
+ * 
+ * @Autowired private UserRepository user;
+ * 
+ * @Autowired private PasswordEncoder passwordEncoder;
+ * 
+ * @Override public UserDetails loadUserByUsername(String u_id) throws
+ * UsernameNotFoundException { UserVO user = this.user.findById(u_id);
+ * List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+ * authorities.add(new SimpleGrantedAuthority("USER"));
+ * 
+ * return new User(user.getUserId(), user.getUserPw(), authorities); }
+ * 
+ * public UserVO save(UserVO vo) {
+ * vo.setUserPw(passwordEncoder.encode(vo.getUserPw())); return user.save(vo); }
+ * 
+ * 
+ * }
+ */
