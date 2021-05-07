56ff5e66bcb677128931070a1b7301b38f968cf8
ds606 <ds606@603-24>
2021-05-07 16:38:16 +0900

    문준호 4차 수정

--- src/main/resources/static/js/insertReply.js
+++ src/main/resources/static/js/insertReply.js
@@ -1,16 +1,24 @@
 window.onload = function() {
 	$('#insertReplyBtn').click(function(e){	 
 		
-	    if(th:text="${userInfo}"=null){
-	        alert("로그인 후, 이용해주세요.");
-	        return false;
-	    }else if($('#replyInsert').val() == ""){
+	   if($('#replyInsert').val() == ""){
 	        alert("댓글을 입력해주세요");
 	        $('#replyInsert').focus();
 	        return false;
 	    }
 	    
-	    this.submit(); 
+	    function commentInsert(insertData){
+	    $.ajax({
+	        url : '/comment/insert',
+	        type : 'post',
+	        data : insertData,
+	        success : function(data){
+	            if(data == 1) {
+	                commentList(); //댓글 작성 후 댓글 목록 reload
+	                $('[name=replyContent]').val('');
+	            }
+	        }
+	    });
 	});
 	
 }
