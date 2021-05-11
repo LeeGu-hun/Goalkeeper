f6929e44fe30246bf5ded6805d44644a255033c4
DS <DS@603-18>
2021-05-11 18:52:55 +0900

    검색진행중

--- src/main/java/goal/controller/MyPageController.java
+++ src/main/java/goal/controller/MyPageController.java
@@ -11,6 +11,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,7 @@ import goal.vo.FriendVO;
 import goal.vo.UserVO;
 
 @RestController
-public class FriendController {
+public class MyPageController {
 	
 	@Autowired
 	public FriendService friendService;
@@ -37,12 +38,13 @@ public class FriendController {
 	
 	@Autowired
 	public UserService userService;
-	
+
 	@GetMapping("/myPage")
 	public ModelAndView openHome(HttpServletRequest request, BoardVO vo) {
 		UserVO user = new UserVO();
 		user = getLoginUser(request);
 		ModelAndView mv = new ModelAndView("view/myPage/myPage_home");
+		
 		if(vo != null) {
 			mv.addObject("vo", user);
 			mv.addObject("uno", user.getUno());
@@ -115,8 +117,8 @@ public class FriendController {
 		friendService.findMyFriend(vo);
 		vo.setFriendName(friendName);
 		
-		List<FriendVO> friendList = friendService.findMyFriend(vo);
-		mv.addObject("friendList", friendList);
+		List<FriendVO> searchFriendList = friendService.findMyFriend(vo);
+		mv.addObject("searchList", searchFriendList);
 		return mv;
 	}
 	
