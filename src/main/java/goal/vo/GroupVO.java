19c7757d176770024c9c574cf8510db1c504fe74
Mrgil0 <dieoxin4@naver.com>
2021-05-17 10:15:26 +0900

    gg

--- src/main/java/goal/vo/GroupVO.java
+++ src/main/java/goal/vo/GroupVO.java
@@ -1,6 +1,8 @@
 package goal.vo;
 
 import java.util.Date;
+import java.util.List;
+
 import org.apache.ibatis.type.Alias;
 import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
@@ -12,4 +14,5 @@ public class GroupVO {
 	private String g_name, g_intro, g_open, g_cate;
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date g_date, g_goaldate;
+	private List<GroupUserVO> groupUser;
 }
