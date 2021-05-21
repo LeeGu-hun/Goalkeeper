2ce8d26c90f6e475174a264b22a778e7c2a42bc6
ds606 <ds606@603-24>
2021-05-21 17:52:26 +0900

    문준호 6시 수정

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -3,4 +3,16 @@ window.onload = function() {
 	 alert("로그인 후 이용해주세요!");
 	   
 	});
+	
+	function openDetailModal(){
+		alert("모달 오픈");
+		$("#noticemodal").fadeIn(300);
+		$("#noticemodal, .modalclose, #confirmbtn").on('click',function(){
+		$("#noticemodal").fadeOut(300);
+		$(".modal-con").fadeOut(300);
+		});
+		
+	};
+	
+
 }
