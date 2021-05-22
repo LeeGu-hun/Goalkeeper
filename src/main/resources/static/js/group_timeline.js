840c64e16f425b25c10cd6ee210fe83f5f4ca456
GIL <GIL@DESKTOP-TM2HJQC>
2021-05-23 04:12:38 +0900

    그룹 수정

--- src/main/resources/static/js/group_timeline.js
+++ src/main/resources/static/js/group_timeline.js
@@ -115,17 +115,3 @@ function fnEdit(bno){
 		$('#modify'+bno).hide();
 	});		
 }
-function openJoin(result){
-	$('.modal-con').attr('style','min-height:24%');
-	if(result == "joinDenied"){
-		openModal("이미 가입된 그룹입니다.");
-	} else if(result =="loginRequire"){
-		openModal("로그인이 필요합니다");
-	} else{
-		var popupWidth = 1215;
-		var popupHeight = 391;
-		var popupX = (window.screen.width / 2) - (popupWidth / 2);
-		var popupY= (window.screen.height / 2) - (popupHeight / 2);
-		window.open('../../group_join', '_blank', 'height=' + popupHeight  + ', width=' + popupWidth  + ', left='+ popupX + ', top='+ popupY);
-	}
-};
