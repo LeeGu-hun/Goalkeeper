a86f3062e06cb312fb9f00859c0e1e2960257906
jm6415 <81296315+jm6415@users.noreply.github.com>
2021-05-25 17:43:18 +0900

    aaa

---
+++ src/main/resources/static/js/sidebanner.js
@@ -0,0 +1,18 @@
+	// 기본 위치(top)값
+		var RfloatPosition = parseInt($("#right_widget").css('top'))
+		var LfloatPosition = parseInt($("#left_widget").css('top'))
+		// scroll 인식
+		$(window).scroll(function() {
+	  
+	    // 현재 스크롤 위치
+	    var currentTop = $(window).scrollTop();
+	    var RbannerTop = currentTop + RfloatPosition + "px";
+	    var LbannerTop = currentTop + LfloatPosition + "px";
+	    //이동 애니메이션
+	    $("#right_widget").stop().animate({
+	      "top" : RbannerTop
+	    }, 300);
+	    $("#left_widget").stop().animate({
+		  "top" : LbannerTop
+		}, 300);
+	}).scroll();
